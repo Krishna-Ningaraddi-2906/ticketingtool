1798b56b8786858cd91d1c9cc1946cb9801c7739
Krishna-Ningaraddi-2906 <krishna.ningaraddi08@gmail.com>
2025-07-02 14:45:12 +0530

    fullstack project setup

---
+++ TicketingTool/frontend/src/App.js
@@ -0,0 +1,13 @@
+import TestComponent from "./Components/TestComponent";
+
+function App() {
+  return (
+    <div>
+      <h1>Ticketing Tool Frontend</h1>
+      {/* all the Components need to be called here */}
+      <TestComponent></TestComponent>
+    </div>
+  );
+}
+
+export default App;
---
+++ TicketingTool/frontend/src/Components/TestComponent.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+
+function TestComponent() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("/api/test")
+      .then(res => res.text())
+      .then(data => setMessage(data))
+      .catch(err => setMessage("❌ Error connecting to backend"));
+  }, []);
+
+  return <h2>{message}</h2>;
+}
+
+export default TestComponent;
